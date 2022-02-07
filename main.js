0e7ac2777675ec65a5bebad3fefb9fcc49fb7580
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-07 12:05:54 +0100

    added HTML page

---
+++ main.js
@@ -0,0 +1,23 @@
+for(const e of document.querySelectorAll('button')) {
+    e.addEventListener('click', fromto);
+}
+
+function fromto() {
+    var from = document.getElementById('from').innerText;
+    var to = document.getElementById('to').innerText;
+    var b = document.createElement('p');
+    if (!isNaN(from) || !isNaN(to)) {
+        document.getElementById("result").innerText = "Bitte gebe eine gültige Zahl ein";
+        return;
+    } else {
+        document.getElementById("result").innerText = "";
+    }
+    var num = 0;
+    var i = from;
+    while (i <= to) {
+        num = num + i;
+        i = i + 1;
+        b.appendChild('div');
+        b.innerText = num;
+    }
+}
