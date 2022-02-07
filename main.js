47ba5d5151b1e2801734b706f8c225af6152061e
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-07 12:40:59 +0100

    changed main.js

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-document.getElementById('button').addEventListener('click', fromto);
+document.getElementsByTagName('button')[0].addEventListener('click', fromto);
 function fromto() {
-    var from = document.getElementById('from').innerText;
-    var to = document.getElementById('to').innerText;
     var b = document.createElement('p');
-    if (!isNaN(from) || !isNaN(to)) {
-        document.getElementById("result").innerText = "Bitte gebe eine gültige Zahl ein";
-        return;
-    } else {
-        document.getElementById("result").innerText = "";
-    }
     var num = 0;
-    var i = from;
-    while (i <= to) {
+    var i = 1;
+    while (i <= 100) {
         num = num + i;
         i = i + 1;
         b.appendChild('div');
