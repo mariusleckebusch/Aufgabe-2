30247605d2c0d8caf1966797e1185e21a98caa1d
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-07 13:02:28 +0100

    added array function

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
-document.getElementsByTagName('button')[0].addEventListener('click', fromto);
+document.getElementsByTagName('button')[0].addEventListener('click', selectfunction);
+
+function selectfunction() {
+    var val = document.getElementById('select').value;
+    if(val == 'FromTo') {fromto()} if(val == 'FromToArray') {fromto2()}
+}
+
+
 function fromto() {
-    var b = document.createElement('p');
-    var num = 0;
-    var i = 1;
-    while (i <= 100) {
-        num = num + i;
-        i = i + 1;
-        b.appendChild('div');
-        b.innerText = num;
+    var from = parseInt(document.getElementById('from').value);
+    var to = parseInt(document.getElementById('to').value);
+    if (isNaN(from) || isNaN(to)) {
+        document.getElementById('result').innerText = "Bitte gebe einen gültigen Wert ein";
+        return;
+    } else {
+        document.getElementById('result').innerText = "";
+        document.getElementById('result').innerHTML = "";
+            var num = 0;
+            var i = from;
+            while (i <= to) {
+                num = num + i;
+                i = i + 1;
+                var b = document.createElement('p');
+                var number = document.createTextNode(num);
+                b.appendChild(number)
+                document.getElementById('result').appendChild(b);
+            }
+            
+    }
+}
+
+function fromto2() {
+var list = [];
+var from = parseInt(document.getElementById('from').value);
+    var to = parseInt(document.getElementById('to').value);
+    if (isNaN(from) || isNaN(to)) {
+        document.getElementById('result').innerText = "Bitte gebe einen gültigen Wert ein";
+        return;
+    } else {
+        document.getElementById('result').innerText = "";
+        document.getElementById('result').innerHTML = "";
+            var num = 0;
+            var i = from;
+            while (i <= to) {
+                num = num + i;
+                i = i + 1;
+                list.push(num);
+            }
+        document.getElementById('result').innerText = list;
     }
 }
