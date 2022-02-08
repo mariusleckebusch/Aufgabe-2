e0e69478f48e381c6ec903971a0825bb0c5d2fd5
mariusleckebusch <marius.leckebusch@482-projects.com>
2022-02-08 09:18:50 +0100

    Lösungweg 2 hinzugefügt

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Aufgabe 2 (Addiere Zahlen von 1 bis 100)
 Schreiben Sie ein Programm, das alle Zahlen von 1 bis 100 summiert. Wählen Sie dafür einen möglichst eleganten Ansatz, implementieren Sie zwei verschiedenee Lösungswege
+
+Lösungsweg 1
+```
+var num = 0;
+var i = 1;
+while (i <= 100) {
+    num = num + i;
+    i = i + 1;
+    console.log(num);
+}
+```
+Lösungsweg 2
+```
+var sum = 0;
+for(var i =1; i <= 100; i++) {
+    sum = sum + i;
+    console.log(sum)
+}
+```
--- main.js
+++ main.js
@@ -1,57 +1,4 @@
-// 1+2+3+4+5+6
-
-document.getElementsByTagName('button')[0].addEventListener('click', selectfunction);
-
-function selectfunction() {
-    var val = document.getElementById('select').value;
-    if(val == 'FromTo') {fromto()} if(val == 'FromToArray') {fromto2()}
-}
-
-
-function fromto() {
-    var from = parseInt(document.getElementById('from').value);
-    var to = parseInt(document.getElementById('to').value);
-    if (isNaN(from) || isNaN(to)) {
-        document.getElementById('result').innerText = "Bitte gebe einen gültigen Wert ein";
-        return;
-    } else {
-        document.getElementById('result').innerText = "";
-        document.getElementById('result').innerHTML = "";
-            var num = 0;
-            var i = from;
-            while (i <= to) {
-                num = num + i;
-                console.log(num+i)
-                i = i + 1;
-                var b = document.createElement('p');
-                var number = document.createTextNode(num);
-                b.appendChild(number)
-                document.getElementById('result').appendChild(b);
-            }
-            
-    }
-}
-
-function fromto2() {
-var list = [];
-var from = parseInt(document.getElementById('from').value);
-    var to = parseInt(document.getElementById('to').value);
-    if (isNaN(from) || isNaN(to)) {
-        document.getElementById('result').innerText = "Bitte gebe einen gültigen Wert ein";
-        return;
-    } else {
-        document.getElementById('result').innerText = "";
-        document.getElementById('result').innerHTML = "";
-            var num = 0;
-            var i = from;
-            while (i <= to) {
-                num = num + i;
-                i = i + 1;
-                list.push(num);
-            }
-        document.getElementById('result').innerText = list;
-    }
-}
+//Lösungsweg 1
 
 var num = 0;
 var i = 1;
@@ -59,4 +6,13 @@ while (i <= 100) {
     num = num + i;
     i = i + 1;
     console.log(num);
+}
+
+
+//Lösungsweg 2
+
+var sum = 0;
+for(var i =1; i <= 100; i++) {
+    sum = sum + i;
+    console.log(sum)
 }
